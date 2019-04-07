d37ce5486f429900bed2b91e347dce59f9dd6adf
DjoleDzele <djoledzele91@gmail.com>
2019-04-07 15:04:01 +0200

    Dodani osnovni kontroleri oglasa i linkovi ka njima

---
+++ MiniOglasi/Controllers/AutoOglasiController.cs
@@ -0,0 +1,12 @@
+﻿using System.Web.Mvc;
+
+namespace MiniOglasi.Controllers
+{
+    public class AutoOglasiController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
