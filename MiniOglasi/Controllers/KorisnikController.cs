ec6d86fbd35a32f4128dd1b803527035000ea006
DjoleDzele <djoledzele91@gmail.com>
2019-04-09 21:54:35 +0200

    samo postavljac oglasa da edituje ili brise oglas

---
+++ MiniOglasi/Controllers/KorisnikController.cs
@@ -0,0 +1,13 @@
+﻿using System.Web.Mvc;
+
+namespace MiniOglasi.Controllers
+{
+    public class KorisnikController : Controller
+    {
+        // GET: Korisnik
+        public ActionResult MojiOglasi()
+        {
+            return View();
+        }
+    }
+}
